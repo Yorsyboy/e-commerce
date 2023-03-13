c16c5a0fb41c5b705fa778861a7a7e75ed1b2f14
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2023-03-13 22:48:21 +0100

    Add header links

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,5 +1,6 @@
-import { AppBar, Container, Toolbar, Typography } from "@mui/material";
+import { AppBar, Container, Link, Toolbar, Typography } from "@mui/material";
 import Head from "next/head";
+import NextLink from "next/link";
 
 export default function Layout({ children }) {
   return (
@@ -10,7 +11,20 @@ export default function Layout({ children }) {
 
       <AppBar position="static" className="bg-[#283040]">
         <Toolbar>
-          <Typography>Ecommerce</Typography>
+          <NextLink href="/" passHref>
+            <Link className="no-underline text-white">
+              <Typography className="font-bold text-lg">Ecommerce</Typography>
+            </Link>
+          </NextLink>
+          <div className="flex-grow"></div>
+          <div className="space-x-4">
+            <NextLink href="/cart" passHref>
+              <Link className="no-underline text-white">Cart</Link>
+            </NextLink>
+            <NextLink href="/login" passHref>
+              <Link className="no-underline text-white">Login</Link>
+            </NextLink>
+          </div>
         </Toolbar>
       </AppBar>
       <Container className="min-h-[80vh]">{children}</Container>
